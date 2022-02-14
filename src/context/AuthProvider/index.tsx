8e1d96ebfc5dc1a3d1719d551fc467fea9c3e131
Gsangi <sanskar6053@gmail.com>
2022-02-15 04:18:27 +0530

    fix : change base url

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -9,7 +9,6 @@ import {
   User,
   getRedirectResult,
   signInWithRedirect,
-  getAdditionalUserInfo,
   FacebookAuthProvider,
 } from 'firebase/auth';
 import { auth, facebookAuthProvider } from 'firebase';
@@ -41,25 +40,34 @@ export const AuthProvider = ({
     getRedirectResult(auth)
       .then((result) => {
         if (result) {
-          const additionalUserInfo = getAdditionalUserInfo(result);
           const credential = FacebookAuthProvider.credentialFromResult(result);
-          auth.currentUser?.getIdToken(true).then((token) => {
-            console.log(credential?.accessToken);
-            console.log(token);
-            if (credential && credential.accessToken && additionalUserInfo) {
-              axios({
-                baseURL: 'https://funny-hound-92.loca.lt',
-                url: '/users',
-                method: 'POST',
+          auth.currentUser?.getIdToken(true).then((res) => {
+            axios
+              .request({
+                url: 'http://localhost:8080/users',
+                method: 'post',
                 headers: {
-                  facebookAccessToken: credential.accessToken,
-                  authToken: token,
-                  isNewUser: additionalUserInfo.isNewUser,
+                  authtoken: res,
+                  facebookaccesstoken: credential?.accessToken as string,
                 },
               })
-                .then((data) => console.log(data))
-                .catch((e) => console.log(e));
-            }
+              .then((ress) => {
+                console.log(ress);
+                axios
+                  .request({
+                    url: 'http://localhost:8080/users',
+                    method: 'get',
+                    headers: {
+                      authtoken: res,
+                    },
+                  })
+                  .then((resss) => {
+                    console.log(resss.data);
+                  });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           });
         }
       })
@@ -72,6 +80,7 @@ export const AuthProvider = ({
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     return auth.onAuthStateChanged(async (newUser) => {
       setUser(newUser);
+      console.log(newUser);
     });
   });
 
@@ -82,24 +91,24 @@ export const AuthProvider = ({
     facebookAuthProvider.addScope(SCOPE_USER_PUBLIC_PROFILE);
     signInWithRedirect(auth, facebookAuthProvider);
   };
-  const userData = () => {
-    auth.currentUser?.getIdToken(true).then((token) => {
-      axios({
-        baseURL: 'https://funny-hound-92.loca.lt',
-        url: '/users',
-        method: 'GET',
-        headers: {
-          authToken: token,
-        },
-      })
-        .then((data) => console.log(data))
-        .catch((e) => console.log(e));
-    });
-  };
+  // const userData = () => {
+  //   auth.currentUser?.getIdToken(true).then((token) => {
+  //     axios({
+  //       baseURL: 'https://funny-hound-92.loca.lt',
+  //       url: '/users',
+  //       method: 'GET',
+  //       headers: {
+  //         authToken: token,
+  //       },
+  //     })
+  //       .then((data) => console.log(data))
+  //       .catch((e) => console.log(e));
+  //   });
+  // };
 
   const value = {
     signInWithFacebook,
-    userData,
+    // userData,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
