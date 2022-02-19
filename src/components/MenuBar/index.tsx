834d14773d12969831cdc4eeb70585a94073f77d
Gsangi <sanskar6053@gmail.com>
2022-02-20 03:58:29 +0530

    feat: add logout button in menu bar

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,20 +1,27 @@
 import { Flex } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from 'context/AuthProvider';
 
-const MenuBar = (): JSX.Element => (
-  <Flex
-    justifyContent="space-evenly"
-    alignItems="center"
-    bg="white"
-    width="95%"
-    alignSelf="center"
-  >
-    <Link to="/">PulpSpit</Link>
-    <Link to="/">Request pending</Link>
-    <Link to="/">Friends available</Link>
-    <Link to="/dashboard">Dashboard</Link>
-    <Link to="/">About us</Link>
-  </Flex>
-);
+const MenuBar = (): JSX.Element => {
+  const { logOut } = useAuthContext();
+  return (
+    <Flex
+      justifyContent="space-evenly"
+      alignItems="center"
+      bg="white"
+      width="95%"
+      alignSelf="center"
+    >
+      <Link to="/">PulpSpit</Link>
+      <Link to="/">Request pending</Link>
+      <Link to="/">Friends available</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/">About us</Link>
+      <Link to="/" onClick={logOut}>
+        Log out
+      </Link>
+    </Flex>
+  );
+};
 
 export default MenuBar;
