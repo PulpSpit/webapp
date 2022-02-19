c1cbe3bce9eb8545d5d44e6cf23a0403eaa63c92
Gsangi <sanskar6053@gmail.com>
2022-02-20 03:58:04 +0530

    feat: make export files for containers and container to wrap loader for whole page

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { ArrowFemaleFriend } from './ArrowFemaleFriend';
+export { ArrowMaleFriend } from './ArrowMaleFriend';
+export { LandingPageLeftReferIcon } from './landingPageLeftReferIcon';
+export { LandingPageRightReferIcon } from './landingPageRightReferIcon';
+export { PulpspitLogo } from './pulpspitLogo';
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Container } from './Container';
+export { default as Loader } from './Loader';
+export { default as MenuBar } from './MenuBar';
+export { default as SubmissionPrompt } from './SubmissionPrompt';
